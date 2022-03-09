ed3aa0279dcbc72c01f01c4a52985782ba9e3b8f
laponte243 <leyner.1002@gmail.com>
2022-03-09 15:36:10 -0400

    primera parte integracion test

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,7 @@
 <template>
-  <v-app dark>
+  <v-app>
     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant="miniVariant"
       :clipped="clipped"
       fixed
       app
@@ -30,55 +29,13 @@
       app
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
       :absolute="!fixed"
       app
@@ -103,15 +60,17 @@ export default {
           to: '/'
         },
         {
+          icon: 'mdi-check',
+          title: 'test',
+          to: '/test'
+        },
+        {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
         }
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      title: "Near's Bookshop"
     }
   }
 }
---
+++ pages/test.vue
@@ -0,0 +1,177 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <v-carousel
+        class="carousel"
+        hide-delimiter-background
+        delimiter-icon="mdi-minus"
+        height="90vh"
+      >
+        <v-carousel-item
+          v-for="(itemC,i) in itemsCarousel"
+          :key="i"
+        >
+          <v-row
+            class="pt-12 mt-12"
+            align="center"
+            justify="center"
+          >
+            <h1 class="black--text pt">
+              Libros en tendencia
+            </h1>
+          </v-row>
+          <v-row
+            align="center"
+            justify="center"
+          >
+            <v-col />
+            <v-col
+              align-self="center"
+            >
+              <div class="text-h3">
+                <a>
+                  <v-img
+                    class="hover imgCarousel"
+                    :src="require(`~/assets/img/${itemC.src1}`)"
+                  />
+                </a>
+              </div>
+            </v-col>
+            <v-col
+              class="d-none d-sm-flex"
+              align-self="center"
+            >
+              <div class="text-h3">
+                <a href="/product">
+                  <v-img
+                    class="hover imgCarousel"
+                    :src="require(`~/assets/img/${itemC.src2}`)"
+                  />
+                </a>
+              </div>
+            </v-col>
+            <v-col
+              class="d-none d-md-flex"
+              align-self="center"
+            >
+              <div class="text-h3">
+                <a href="/product">
+                  <v-img
+                    class="hover imgCarousel"
+                    :src="require(`~/assets/img/${itemC.src3}`)"
+                  />
+                </a>
+              </div>
+            </v-col>
+            <v-col />
+          </v-row>
+        </v-carousel-item>
+      </v-carousel>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage',
+  data () {
+    return {
+      itemsCarousel: [
+        {
+          src1: 'home/libro2.jpg',
+          src2: 'home/libro3.jpg',
+          src3: 'home/libro4.jpg'
+        },
+        {
+          src1: 'home/libro5.jpg',
+          src2: 'home/libro6.jpg',
+          src3: 'home/libro7.jpg'
+        },
+        {
+          src1: 'home/libro8.jpg',
+          src2: 'home/libro9.jpg',
+          src3: 'home/libro1.png'
+        },
+        {
+          src1: 'home/libro4.jpg',
+          src2: 'home/libro5.jpg',
+          src3: 'home/libro6.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style>
+  .v-card--reveal {
+    align-items: center;
+    bottom: 0;
+    justify-content: center;
+    opacity: .5;
+    position: absolute;
+    width: 100%;
+  }
+  .carousel {
+    background-image: url("~assets/img/home/fondoCarousel.png");
+    background-size: 101% 100%;
+  }
+  .hover:hover {
+    transform: scale(1.1);
+    box-shadow: 3px 3px 30px 15px gray;
+  }
+  .hover2:hover {
+    transform: scale(1);
+    box-shadow: 3px 3px 10px 5px gray;
+  }
+  .v-btn .v-btn__content .v-icon {
+    color: gray;
+  }
+  .imgCarousel {
+    width: 250px;
+    height: 400px;
+  }
+  .generoImg {
+    height: 400px;
+  }
+  .imgOffers {
+    width: 265px;
+    height: 350px;
+  }
+  .imgPublica {
+    width: 500px;
+  }
+  @media (min-width:320px) and (max-width:425px){
+    /*Aquí van todos los estilos CSS*/
+    .carousel {
+      background-image: url("~assets/img/home/fondoCarousel.png");
+      /*(width heigth)*/
+      background-size: 101% 100%;
+    }
+    .generoImg {
+      height: 200px;
+    }
+  };
+  @media (min-width:375px) and (max-width:425px){
+    /*Aquí van todos los estilos CSS*/
+  }
+  @media (max-width:768px) {
+    /*Aquí van todos los estilos CSS*/
+    .imgPublica {
+      width: 300px;
+    }
+  }
+  @media (min-width:960px) and (max-width:1024px){
+    /*Aquí van todos los estilos CSS*/
+    .carousel {
+      background-image: url("~assets/img/home/fondoCarousel.png");
+      /*(width heigth)*/
+      background-size: 101% 100%;
+    }
+  };
+  @media (orientation: landscape) {
+    /*Aquí van todos los estilos CSS*/
+  }
+  @media (orientation: Portrait) {
+    /*Aquí van todos los estilos CSS*/
+  }
+</style>
